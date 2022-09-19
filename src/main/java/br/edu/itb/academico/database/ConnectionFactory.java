356b3b030da029f2206062c47fd3029fca8824a4
Mikael <rm85700@estudante.fieb.edu.br>
2022-09-19 20:44:37 -0300

    Enviando os endereços de conexões

--- src/main/java/br/edu/itb/academico/database/ConnectionFactory.java
+++ src/main/java/br/edu/itb/academico/database/ConnectionFactory.java
@@ -10,7 +10,8 @@ import java.sql.SQLException;
 public abstract class ConnectionFactory {
 	
 	public static Connection conn = null;
-	private final static String  URLSQLSERVER = "jdbc:sqlserver://DESKTOP-L0N3OP9\\FTZ;user=sa;password=123456;databaseName=InfoPosto";
+	private final static String  URLSQLSERVER = "jdbc:sqlserver://localhost:1433;"
+			+ "user=sa;password=@ITB123456;databaseName=InfoPosto";
 	
 
 	public static Connection getConnectionSqlServer () throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
