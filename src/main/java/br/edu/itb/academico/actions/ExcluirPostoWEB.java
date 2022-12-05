2c026f1c62eb10037e1430a1b3700acb1e354592
Nilton Mikael <niltonpalmeira@sisconsultoria.com.br>
2022-12-04 21:31:57 -0300

    Tela de Cadastro de Admin

---
+++ src/main/java/br/edu/itb/academico/actions/ExcluirPostoWEB.java
@@ -0,0 +1,32 @@
+package br.edu.itb.academico.actions;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.edu.itb.academico.database.ConnectionFactory;
+
+public class ExcluirPostoWEB implements Action  {
+
+		@Override
+		public String executa(HttpServletRequest request, HttpServletResponse response) throws Exception {
+			
+		try {
+			int Codigo = Integer.parseInt(request.getParameter("codigo"));
+			
+			Connection con = ConnectionFactory.getConnectionSqlServer();
+//			
+			PreparedStatement ps = con.prepareStatement("DELETE FROM tbPosto WHERE Codigo =?");
+			ps.setInt(1, Codigo);
+			ps.execute();
+			return "/WEB-INF/jsp/ListaDePostos/ListaDePostos.jsp";
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+			return "/WEB-INF/jsp/ListaDePostos/ListaDePostos.jsp";
+
+		}
+			
+		}
+}
